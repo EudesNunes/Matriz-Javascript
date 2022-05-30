dbc9d1d5a9995e8dcc18334932c09cc842b5776a
Eudes <67762805+EudesNunes@users.noreply.github.com>
2022-05-30 19:58:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var matriz = [[1,3,3,4],
+var matriz = [[1,2,3,4],
               [5,6,7,8],
               [9,10,11,12],
               [13,14,15,16]]
 var soma = 0
 var somacoluna = 0
-var produto = 0
-var produtolinha = 0
+var produto = 1
+var produtolinha = 1
  for(var i = 0; i < matriz.length; i++) {
-    for(var j = 0; j < matriz.length; j++) {
-      
-    soma += matriz[i][j];
-      
-    somacoluna = matriz[0][0] + matriz[1][0] + matriz[2][0] + matriz[3][0]
-      
-    produtolinha = matriz[0][0] * matriz[0][1] * matriz[0][2] * matriz[0][3]
-      
-    produto = matriz[0][0] * matriz[1][1] * matriz[2][2] * matriz[3][3]
-      
+    for(var j = 0; j < matriz.length; j++) {   
+    soma += matriz[i][j];      
     }
  }
+for(var i = 0; i < matriz.length; i++) {
+   somacoluna += matriz[i][0]
+}
+for(var j = 0; j < matriz.length; j++) {
+   produtolinha *= matriz[0][j]
+}
+for(var i = 0; i < matriz.length; i++) {
+   produto *= matriz[i][i]
+}
 
 console.log('1- A soma dos elementos de primeira coluna:',somacoluna)
 
